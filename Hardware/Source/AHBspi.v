fbb7a9d7b63b26c17f1512207a4d5a8548c4e0b9
Unknown <eoinoconn@gmail.com>
2018-04-16 16:06:13 +0100

    sorta fixed

---
+++ Hardware/Source/AHBspi.v
@@ -0,0 +1,92 @@
+module AHBspi(
+			// Bus signals
+			input wire HCLK,				// bus clock
+			input wire HRESETn,			// bus reset, active low
+			input wire HSEL,				// selects this slave
+			input wire HREADY,			// indicates previous transaction completing
+			input wire [31:0] HADDR,	// address
+			input wire [1:0] HTRANS,	// transaction type (only bit 1 used)
+			input wire HWRITE,			// write transaction
+//			input wire [2:0] HSIZE,		// transaction width ignored
+			input wire [31:0] HWDATA,	// write data
+			output wire [31:0] HRDATA,	// read data from slave
+			output wire HREADYOUT,		// ready output from slave
+			// SPI signals
+			input MISO,				    //  
+			output MOSI,				// 
+			output SCLK,				// 
+			output SSn                  //
+			
+    );
+
+        // Registers to hold signals from address phase
+        reg [1:0] rHADDR;            // only need two bits of address
+        reg rWrite, rRead;    // write enable signals
+    
+    
+    
+        // Internal signals
+        reg [7:0]    readData;        // 8-bit data from read multiplexer
+        //wire [7:0] rx_fifo_out, rx_fifo_in, tx_fifo_out;  // fifo data
+        //wire rx_fifo_empty, rx_fifo_full, tx_fifo_empty, tx_fifo_full;  // fifo output signals
+        wire tx_wr = rWrite & (rHADDR == 2'h2);  // tx fifo write on write to address 0x4
+        //wire rx_fifo_rd = (rRead & (rHADDR == 2'h1) & ~rx_fifo_empty);  // rx fifo read on read to address 0x0
+        wire txrdy;        // transmitter status signal
+        //wire txgo = ~tx_fifo_empty;    // transmitter control signal
+        //wire rxnew;        // receiver strobe output
+    
+         // Capture bus signals in address phase
+        always @(posedge HCLK)
+            if(!HRESETn)
+                begin
+                    rHADDR <= 2'b0;
+                    rWrite <= 1'b0;
+                    rRead  <= 1'b0;
+                end
+            else if(HREADY)
+             begin
+                rHADDR <= HADDR[3:2];         // capture address bits for for use in data phase
+                rWrite <= HSEL & HWRITE & HTRANS[1];    // slave selected for write transfer       
+                rRead <= HSEL & ~HWRITE & HTRANS[1];    // slave selected for read transfer 
+             end
+    
+            
+        // Status bits - can read in status register, can cause interrupts if enabled
+        wire status = ~tx_ready;
+        
+            
+        // Bus output signals
+        always @(rxdout, status, rHADDR)
+            case (rHADDR)        // select on word address (stored from address phase)
+                2'h0:        readData = {7'b0, status};    // status register    
+                2'h1:        readData = rxdout;    // read from rx fifo - oldest received byte
+                //2'h2:        readData = txdout;    // read of tx register gives oldest byte in queue    
+                default:     readData = {8'b0};
+            endcase
+            
+        assign HRDATA = {24'b0, readData};    // extend with 0 bits for bus read
+    
+    // Options on ready signal - can wait on write when full, or read when empty 
+        assign HREADYOUT = 1'b1;    // always ready - transaction never delayed
+    //    assign HREADYOUT = ~((tx_fifo_wr & tx_fifo_full) | (rx_fifo_rd & rx_fifo_empty));
+        
+
+    // ========================= SPI ===================================================
+    
+    wire [7:0] rxdout;
+    // Spi module
+    SPI    rSPI (
+              .clk         (HCLK),
+              .rst         (~HRESETn),
+              .txdin       (HWDATA[7:0]),
+              .txgo        (tx_wr),
+              .txrdy       (tx_ready),
+              .rxdout      (rxdout),
+              .MISO        (MISO),               // serial receive, idles at 1
+              .MOSI        (MOSI),               // serial transmit, idles at 1
+              .SCLK        (SCLK),               // interrupt request
+              .SSn         (SSn) 
+            );
+    
+endmodule
+    
---
+++ Hardware/Source/spi.v
@@ -0,0 +1,95 @@
+`timescale 1ns / 1ps
+
+module SPI(
+    input wire clk,						// main clock, drives all logic
+    input wire rst,						// asynchronous reset
+    input wire [7:0] txdin,			// 8-bit data to be transmitted
+    input wire txgo,					// indicates new data to send, ignored if not ready
+    output reg MOSI,				// serial data out (idle at logic 1, high)
+    output wire SSn,
+    output wire SCLK,
+    output txrdy,					// transmitter ready for new data
+    input MISO,						// serial data in (idle at logic 1, high)
+    output reg [7:0] rxdout	// 8-bit received data
+    //output wire rxnew					// indicates new data available, asserted for 1 clock
+ );
+    
+    ////////////////////////////////////////////////////////
+    // SCLK counter
+    ////////////////////////////////////////////////////////
+    reg [3:0] count;
+    wire [3:0] next_count = count + 4'b1;
+    
+    always @(posedge clk or posedge rst)
+        if (rst)
+            count <= 4'b0;
+        else if (bitcount == 4'b0)
+            count <= 4'b0;
+        else
+            count <= next_count;
+            
+    assign SCLK = (count[3]);
+    
+
+    ////////////////////////////////////////////////////////
+    // SSn controller
+    ////////////////////////////////////////////////////////
+
+    assign SSn = (bitcount == 4'b0);
+    
+    ////////////////////////////////////////////////////////
+    // input data capture
+    ////////////////////////////////////////////////////////
+    reg [7:0] datareg;
+    
+    always @(posedge clk or posedge rst)
+        if (rst)
+            datareg <= 8'b0;
+        else if (txgo & txrdy)
+            datareg <= txdin;
+          
+
+    ////////////////////////////////////////////////////////
+    // MOSI control
+    ////////////////////////////////////////////////////////
+    reg [3:0] bitcount;
+    reg [3:0] bitcount_decrement;
+    assign txrdy = (bitcount == 4'b0);
+    
+    always@(posedge SCLK)
+        bitcount_decrement = bitcount - 4'b1; 
+    
+    always @(posedge clk or posedge rst)
+        if (rst)
+            bitcount <= 4'b0;
+        else if (txgo & txrdy)
+            bitcount <= 4'd9;
+        else if (count == 4'd8)
+            bitcount <= bitcount_decrement;
+    
+    always @(bitcount, datareg)
+        case(bitcount)		
+            4'd8:        MOSI = datareg[0];    
+            4'd7:        MOSI = datareg[1];
+            4'd6:        MOSI = datareg[2];
+            4'd5:        MOSI = datareg[3];
+            4'd4:        MOSI = datareg[4];
+            4'd3:        MOSI = datareg[5];
+            4'd2:        MOSI = datareg[6];
+            4'd1:        MOSI = datareg[7];    
+            4'd0:        MOSI = 1'b0;            
+            default:     MOSI = 1'b0;
+         endcase
+
+    ////////////////////////////////////////////////////////
+    // MIS0 control
+    //////////////////////////////////////////////////////// 
+    
+    always @ (posedge SCLK or posedge rst)
+        if (rst) rxdout <= 8'b0;                // reset to zero
+        else                                      // on sampling pulse 
+            rxdout <= {MISO, rxdout[7:1]};     // shift right...
+        
+    
+   endmodule
+   
